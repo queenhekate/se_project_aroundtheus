b09885c534873aad39971cd2847eedcf5db54291
JOHANNA SCHNELL <JOHANNASCHNELL@GMAIL.COM>
2024-07-18 23:41:42 -0700

    new photos

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    name: "Minnehaha Falls",
+    link: "https://www.planetware.com/wpimages/2019/12/minnesota-in-pictures-beautiful-places-to-photograph-minnehaha-falls.jpg",
   },
   {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    name: "Boundary Waters Canoe Area Wilderness",
+    link: "https://www.minnpost.com/wp-content/uploads/2022/03/SouthTemperanceLake940.jpg",
   },
   {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    name: "Split Rock Lighthouse",
+    link: "https://www.greatlakescruises.com/uploads/1/0/6/4/106440933/duluth-split-rock-lighthouse-3_orig.jpg",
   },
   {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    name: "Mississippi River",
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Efmo_View_from_Fire_Point.jpg/640px-Efmo_View_from_Fire_Point.jpg",
   },
   {
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    name: "Voyageurs National Park",
+    link: "https://www.planetware.com/wpimages/2019/12/minnesota-in-pictures-beautiful-places-to-photograph-voyageurs-national-park.jpg",
   },
   {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    name: "Balsam Township",
+    link: "https://www.compass.com/m/f6d367cc15ca0025f0d4911cccdd6befe350606b_img_0_4db8e/640x480.jpg",
   },
 ];
 
