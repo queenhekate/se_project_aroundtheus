375eb78ffea7767b012e4f165aa7f613442a43a5
JOHANNA SCHNELL <JOHANNASCHNELL@GMAIL.COM>
2024-09-29 17:24:22 -0700

    code review

--- src/pages/index.js
+++ src/pages/index.js
@@ -115,7 +115,7 @@ function handleLikeButton(cardElement) {
       .likeCard(cardElement._id)
       .then(() => {
         cardElement.isLiked = true;
-        cardElemen.handleLikeIcon();
+        cardElement.handleLikeIcon();
       })
       .catch(console.error);
   } else {
@@ -123,7 +123,7 @@ function handleLikeButton(cardElement) {
       .unlikeCard(cardElement._id)
       .then(() => {
         cardElement.isLiked = false;
-        cardElemen.handleLikeIcon();
+        cardElement.handleLikeIcon();
       })
       .catch(console.error);
   }
