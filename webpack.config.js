56b37393d1a9c40e3d0b9fe8b77f9503902225fc
Johanna Schnell <johannaschnell@gmail.com>
2025-05-20 14:14:06 -0700

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "",
+    publicPath: "./",
   },
   target: ["web", "es5"],
   stats: "errors-only",
